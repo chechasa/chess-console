a4c7247b1e60f57061ce6abe40c75cd52158d9c2
Maickel <maickeldmc3@gmail.com>
2021-05-17 18:35:12 -0300

    Começando a implementar o tabuleiro

--- xadrez_console/Program.cs
+++ xadrez_console/Program.cs
@@ -1,17 +1,18 @@
 ﻿using System;
 using tabuleiro;
+using tabuleiro.Enum;
+
 namespace xadrez_console
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Posicao p = new Posicao(3, 4);
-
-            Console.WriteLine("Posição "+ p);
-            Console.ReadLine();
+            Tabuleiro t = new Tabuleiro(8,8);
+            Console.WriteLine(t);
+            
         }
 
-        
+
     }
 }
--- xadrez_console/tabuleiro/Posicao.cs
+++ xadrez_console/tabuleiro/Posicao.cs
@@ -9,11 +9,19 @@ namespace tabuleiro
         public int Linha { get; set; }
         public int Coluna { get; set; }
 
+        public Posicao()
+        {
+        }
         public Posicao(int linha, int coluna)
         {
             Linha = linha;
             Coluna = coluna;
         }
-        
+        public override string ToString()
+        {
+            return Linha
+                + ", "
+                + Coluna;
+        }
     }
 }
